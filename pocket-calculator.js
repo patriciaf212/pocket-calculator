6de28628520efc1af6b7ef5522777c164c2754f2
Patricia Falar <patriciafalar212@gmail.com>
2019-01-04 10:46:43 -0500

    display

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,26 @@
+let p = document.getElementById("display");
+let str = ""
+p.innerHTML= 0;
+
+function one() {
+  let p = document.getElementById("display");
+  str += "0";
+  p.innerHTML = str;
+}
+
 function AC() {
 
+}
+
+
+function addition() {
+  let sum = 0;
+  let addNumber = "";
+  let numberAdd = "";
+
+  sum = addNumber + numberAdd;
+}
+
+function one() {
+  let one = 1;
 }
