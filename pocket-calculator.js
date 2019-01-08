8166b054a9023bd269ac06def3f6a38fde9faa6d
Patricia Falar <patriciafalar212@gmail.com>
2019-01-08 10:48:34 -0500

    buttons

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -4,14 +4,105 @@ p.innerHTML= 0;
 
 function one() {
   let p = document.getElementById("display");
+  str += "1";
+  p.innerHTML = str;
+}
+
+function two() {
+  let p = document.getElementById("display");
+  str += "2";
+  p.innerHTML = str;
+}
+
+function three() {
+  let p = document.getElementById("display");
+  str += "3";
+  p.innerHTML = str;
+}
+
+function four() {
+  let p = document.getElementById("display");
+  str += "4";
+  p.innerHTML = str;
+}
+
+function five() {
+  let p = document.getElementById("display");
+  str += "4";
+  p.innerHTML = str;
+}
+
+function six() {
+  let p = document.getElementById("display");
+  str += "6";
+  p.innerHTML = str;
+}
+
+function seven() {
+  let p = document.getElementById("display");
+  str += "7";
+  p.innerHTML = str;
+}
+
+function eight() {
+  let p = document.getElementById("display");
+  str += "8";
+  p.innerHTML = str;
+}
+
+function nine() {
+  let p = document.getElementById("display");
+  str += "9";
+  p.innerHTML = str;
+}
+
+function zero() {
+  let p = document.getElementById("display");
   str += "0";
   p.innerHTML = str;
 }
 
-function AC() {
-
+function addition() {
+  let p = document.getElementById("display");
+  str += "+";
+  p.innerHTML = str;
 }
 
+function subtraction() {
+  let p = document.getElementById("display");
+  str += "-";
+  p.innerHTML = str;
+}
+
+function multiplication() {
+  let p = document.getElementById("display");
+  str += "x";
+  p.innerHTML = str;
+}
+
+function division() {
+  let p = document.getElementById("display");
+  str += "/";
+  p.innerHTML = str;
+}
+
+function decimal() {
+  let p = document.getElementById("display");
+  str += ".";
+  p.innerHTML = str;
+}
+
+function percentage() {
+  let p = document.getElementById("display");
+  str += "% ";
+  p.innerHTML = str;
+}
+
+function equal() {
+  let p = document.getElementById("display");
+  str += "=";
+  p.innerHTML = str;
+}
 
 function addition() {
   let sum = 0;
@@ -19,8 +110,4 @@ function addition() {
   let numberAdd = "";
 
   sum = addNumber + numberAdd;
-}
-
-function one() {
-  let one = 1;
 }
