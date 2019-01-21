74557e824f9d45739bfb2ceeb32f17374df205bc
Patricia Falar <patriciafalar212@gmail.com>
2019-01-20 22:24:53 -0500

    need commas

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,60 +1,72 @@
 let str = "";
 let val = 0;
-let c = 1;
 let str2 = "";
+let c = 1;
+
+
+
 function one() {
   let p = document.getElementById("display");
   str += "1";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function two() {
   let p = document.getElementById("display");
   str += "2";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function three() {
   let p = document.getElementById("display");
   str += "3";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function four() {
   let p = document.getElementById("display");
   str += "4";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function five() {
   let p = document.getElementById("display");
   str += "5";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function six() {
   let p = document.getElementById("display");
   str += "6";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function seven() {
   let p = document.getElementById("display");
   str += "7";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function eight() {
   let p = document.getElementById("display");
   str += "8";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
 }
 
 function nine() {
   let p = document.getElementById("display");
   str += "9";
+  if (str.length > 9) str = str.substring(0,9);
   p.innerHTML = str;
-}
+  }
 
 function zero() {
   let p = document.getElementById("display");
@@ -65,51 +77,51 @@ function zero() {
 function addition() {
   let p = document.getElementById("display");
     if (c === 1) {
-      str2 = str
-      c++;
+      str2 = str;
+      c++
       str = "";
     }
     str2 += "+";
-    p.innerHTML = str;
+    p.innerHTML = " ";
 }
 
 function subtraction() {
   let p = document.getElementById("display");
   if (c === 1) {
-    str2 = str
-    c++;
+    str2 = str;
+    c++
     str = "";
   }
   str2 += "-";
-  p.innerHTML = 0;
+  p.innerHTML = " ";
 }
 
 function multiplication() {
   let p = document.getElementById("display");
   if (c === 1) {
-    str2 = str
-    c++;
+    str2 = str;
+    c++
     str = "";
   }
   str2 += "*";
-  p.innerHTML = 0;
+  p.innerHTML = " ";
 }
 
 function division() {
   let p = document.getElementById("display");
   if (c === 1) {
-    str2 = str
-    c++;
+    str2 = str;
+    c++
     str = "";
   }
   str2 += "/";
-  p.innerHTML = 0;
+  p.innerHTML = " ";
 }
-
 
 function decimal() {
   let p = document.getElementById("display");
   str += ".";
+  if (str.length > 2) str = str.substring(0,2);
   p.innerHTML = str;
 }
 
@@ -133,16 +145,17 @@ function equal() {
     p.innerHTML = exponent;
   }
 
-
-//  if (result === "infinity") {
-    //  p.innerHTML = "ERROR";
-    //}
+  if (result === " ") {
+      result = ERROR;
+    }
   }
 
-//function negation() {
-  //  let p = document.getElementById("display");
-  //  val = -val;
-  //}
+function negation() {
+  let p = document.getElementById("display");
+  str += "-"
+  p.innerHTML = str;
+  val = 0 - val;
+  }
 
 function percentage() {
   let p = document.getElementById("display");
@@ -150,9 +163,3 @@ function percentage() {
     str = str*(.01);
     p.innerHTML = str;
   }
-
-//function decimal() {
-  //if (x > 999) {
-    //let p = document.getElementById("display").value = y.toLocalString();
-  //}
-//}
